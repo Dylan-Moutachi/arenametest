76f6ee31c01eb8c66ef5cd4e46a196f64fccc80d
Dylan Moutachi <167035865+Dylan-Moutachi@users.noreply.github.com>
2025-05-23 18:58:11 +0200

    chore: double quotes instead of single quotes

--- app/services/csv_reader_service.rb
+++ app/services/csv_reader_service.rb
@@ -1,11 +1,11 @@
-require 'charlock_holmes'
+require "charlock_holmes"
 
 class CsvReaderService
   def self.convert_to_utf8(content)
     detection = CharlockHolmes::EncodingDetector.detect(content)
     raise StandardError, "Encoding could not be detected" unless detection&.dig(:encoding)
 
-    CharlockHolmes::Converter.convert(content, detection[:encoding], 'UTF-8')
+    CharlockHolmes::Converter.convert(content, detection[:encoding], "UTF-8")
   rescue StandardError => e
     raise StandardError, "Failed to convert to UTF-8: #{e.message}"
   end
