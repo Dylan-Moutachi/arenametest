35935a70f855fac3bb30022dcfbf5f0414c52dc2
Dylan Moutachi <167035865+Dylan-Moutachi@users.noreply.github.com>
2025-05-24 16:01:40 +0200

    feat: csv mapping

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,49 +1,35 @@
 require "csv"
 
 class Booking < ApplicationRecord
-  # regarding sample csv, age and gender are not always provided
-  # so I suppose these are not mandatory informations.
+  # Some fields like age and gender are not always provided
   validates :ticket_number, :booking_number, :booking_date, :booking_hour,
-          :event_key, :event, :show_key, :show, :show_date, :show_hour,
-          :show_end_date, :show_end_hour, :price,
-          :product_type, :sales_channel,
-          :first_name, :last_name, :email,
-          :address, :postal_code, :country,
-          presence: true
+            :event_key, :event, :show_key, :show, :show_date, :show_hour,
+            :show_end_date, :show_end_hour, :price,
+            :product_type, :sales_channel,
+            :first_name, :last_name, :email,
+            :address, :postal_code, :country,
+            presence: true
+
+  # Ticket logically seems unique
   validates :ticket_number, uniqueness: true
 
-  def self.import(file)
+  def self.import(file, csv_mapping: {})
     successes = 0
     errors = []
-    # Ideally, we can variabilize CSV parsing arguments
-    # CSV.foreach(file.path, **options) ...
+
     CSV.foreach(file.path, headers: true, col_sep: ";", encoding: "utf-8") do |row|
       begin
-        Booking.create!(
-          ticket_number: row["Numero billet"],
-          booking_number: row["Reservation"],
-          booking_date: row["Date reservation"],
-          booking_hour: row["Heure reservation"],
-          event_key: row["Cle spectacle"],
-          event: row["Spectacle"],
-          show_key: row["Cle representation"],
-          show: row["Representation"],
-          show_date: row["Date representation"],
-          show_hour: row["Heure representation"],
-          show_end_date: row["Date fin representation"],
-          show_end_hour: row["Heure fin representation"],
-          price: row["Prix"],
-          product_type: row["Type de produit"],
-          sales_channel: row["Filiere de vente"],
-          first_name: row["Prenom"],
-          last_name: row["Nom"],
-          email: row["Email"],
-          address: row["Adresse"],
-          postal_code: row["Code postal"],
-          country: row["Pays"],
-          age: row["Age"],
-          gender: row["Sexe"]
-        )
+        # Dynamically map CSV columns to model attributes
+        attributes = {}
+
+        csv_mapping.each do |model_attr, csv_column_name|
+          attributes[model_attr] = row[csv_column_name]
+        end
+
+        # Store mapping used for this import in the jsonb column
+        attributes[:csv_mapping] = csv_mapping
+
+        Booking.create!(attributes)
         successes += 1
       rescue ActiveRecord::RecordInvalid => e
         Rails.logger.error "Ignored line: #{row.to_h} – error: #{e.message}"
@@ -51,6 +37,6 @@ class Booking < ApplicationRecord
       end
     end
 
-    { successes:, errors: }
+    { successes: successes, errors: errors }
   end
 end
